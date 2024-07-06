d7ded3af71393c78dc8c9b684143fa6dabaa67cf
mostafa-j303 <mostafajarjour18@gmail.com>
2024-07-06 18:32:33 +0300

    appointemt

--- src/app/_components/Appointment.tsx
+++ src/app/_components/Appointment.tsx
@@ -1,113 +1,57 @@
 import Link from "next/link";
 import React from "react";
+import data from "../../../public/data.json";
 
 function Appointment() {
   return (
-    <div id="appointment" className="bg-gradient-to-b from-hovprimary via-white to-hovsecondary mx-auto  shadow-2xl shadow-primary px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-      <div  className=" grid grid-cols-1 gap-4 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 sm:items-center md:gap-8">
-        <div className="bg-white rounded-2xl border border-gray-300 p-6 shadow-sm sm:px-8 lg:p-12 hover:border-primary hover:border-2 transition duration-500 hover:scale-y-105">
-          <div className="text-center">
-            <h2 className="text-lg font-medium text-gray-900">
-              Starter
-              <span className="sr-only">Plan</span>
-            </h2>
+    <div id="appointment" className="bg-gradient-to-b from-hovprimary via-white to-hovsecondary mx-auto shadow-2xl shadow-primary px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 sm:items-center md:gap-8">
+        {data.appointment.map((appointment) => (
+          <div key={appointment.id} className="bg-white rounded-2xl border border-gray-300 p-6 shadow-sm sm:px-8 lg:p-12 hover:border-primary hover:border-2 transition duration-500 hover:scale-y-105">
+            <div className="text-center">
+              <h2 className="text-lg font-medium text-gray-900">
+                {appointment.name}
+                <span className="sr-only">Plan</span>
+              </h2>
 
-            <p className="mt-2 sm:mt-4">
-              <strong className="text-3xl font-bold text-gray-900 sm:text-4xl">
-                {" "}
-                20${" "}
-              </strong>
+              <p className="mt-2 sm:mt-4">
+                <strong className="text-3xl font-bold text-gray-900 sm:text-4xl">
+                  {appointment.price}
+                </strong>
+                <span className="text-sm font-medium text-gray-700">/{appointment.duration}</span>
+              </p>
+            </div>
 
-              <span className="text-sm font-medium text-gray-700">/month</span>
-            </p>
+            <ul className="mt-6 space-y-2">
+              {appointment.details.map((detail, index) => (
+                <li key={index} className="flex items-center gap-1">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-5 text-primary"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4.5 12.75l6 6 9-13.5"
+                    />
+                  </svg>
+                  <span className="text-gray-700">{detail}</span>
+                </li>
+              ))}
+            </ul>
+
+            <Link
+              href="#"
+              className="mt-8 block rounded-full border border-primary bg-white px-12 py-3 text-center text-sm font-medium text-primary hover:ring-1 hover:ring-primary focus:outline-none focus:ring active:text-primary hover:text-white hover:bg-primary transition duration-500"
+            >
+              Get Started
+            </Link>
           </div>
-
-          <ul className="mt-6 space-y-2">
-            <li className="flex items-center gap-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-5 text-primary"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.5 12.75l6 6 9-13.5"
-                />
-              </svg>
-
-              <span className="text-gray-700"> 10 users included </span>
-            </li>
-
-            <li className="flex items-center gap-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-5 text-primary"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.5 12.75l6 6 9-13.5"
-                />
-              </svg>
-
-              <span className="text-gray-700"> 2GB of storage </span>
-            </li>
-
-            <li className="flex items-center gap-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-5 text-primary"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.5 12.75l6 6 9-13.5"
-                />
-              </svg>
-
-              <span className="text-gray-700"> Email support </span>
-            </li>
-
-            <li className="flex items-center gap-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-5 text-primary"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.5 12.75l6 6 9-13.5"
-                />
-              </svg>
-
-              <span className="text-gray-700"> Help center access </span>
-            </li>
-          </ul>
-
-          <Link
-            href="#"
-            className="mt-8 block rounded-full border border-primary bg-white px-12 py-3 text-center text-sm font-medium text-primary hover:ring-1 hover:ring-primary focus:outline-none focus:ring active:text-primary hover:text-white hover:bg-primary transition duration-500"
-          >
-            Get Started
-          </Link>
-        </div>
-       
+        ))}
       </div>
     </div>
   );
