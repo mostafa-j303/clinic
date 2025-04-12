c96ad865e0c04aee4c4f14b12e8f3a097f76f6a6
mostafa-j303 <mostafajarjour18@gmail.com>
2025-04-13 02:41:38 +0300

    stayl 13/4/25

--- src/app/_components/Appointment.tsx
+++ src/app/_components/Appointment.tsx
@@ -51,24 +51,24 @@ function Appointment() {
 
   return (
     <div id="appointment" className="bg-gradient-to-b from-hovprimary via-white to-hovsecondary mx-auto shadow-2xl shadow-primary px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-      <div className="grid grid-cols-1  gap-4 sm:grid-cols-3  md:grid-cols-3 lg:grid-cols-3  md:gap-8">
+      <div className="grid grid-cols-2  gap-4 sm:grid-cols-3  md:grid-cols-3 lg:grid-cols-3  md:gap-8">
         {data.appointment.map((appointment: AppointmentType) => (
-          <div key={appointment.id} className="flex flex-col justify-around  bg-white hover:box-content rounded-2xl border border-gray-300 p-2 pb-4 shadow-sm   hover:border-primary hover:border-2 transition duration-500 hover:scale-y-105">
+          <div key={appointment.id} className="flex flex-col justify-between  bg-white hover:box-content rounded-2xl border border-gray-300 p-2 pb-4 shadow-sm   hover:border-primary hover:border-2 transition duration-500 hover:scale-y-105">
             <div className="text-center">
               <h2 className="text-lg font-medium text-gray-900">
                 {appointment.name}
                 <span className="sr-only">Plan</span>
               </h2>
 
-              <p className="mt-2 sm:mt-4">
-                <strong className="text-3xl font-bold text-gray-900 sm:text-4xl">
+              <p className="mt-1 sm:mt-4">
+                <strong className="text-xl font-bold text-primary sm:text-4xl">
                   {appointment.price}
                 </strong>
                 <span className="text-sm font-medium text-gray-700">/{appointment.duration}</span>
               </p>
             </div>
 
-            <ul className="mt-6 space-y-2">
+            <ul className="mt-1 space-y-1">
               {appointment.details.map((detail, index) => (
                 <li key={index} className="flex items-center gap-1">
                   <svg
@@ -77,7 +77,7 @@ function Appointment() {
                     viewBox="0 0 24 24"
                     strokeWidth="1.5"
                     stroke="currentColor"
-                    className="size-5 text-primary"
+                    className="size-3 text-primary"
                   >
                     <path
                       strokeLinecap="round"
@@ -85,13 +85,13 @@ function Appointment() {
                       d="M4.5 12.75l6 6 9-13.5"
                     />
                   </svg>
-                  <span className="text-gray-700">{detail}</span>
+                  <span className="text-gray-500 text-xs ">{detail}</span>
                 </li>
               ))}
             </ul>
               <div className="flex justify-end flex-col">  <button
               onClick={() => openModal(appointment)}
-              className="w-fit self-center mt-8 block rounded-full border border-primary bg-white px-12 py-3 text-center text-sm font-medium text-primary hover:ring-1 hover:ring-primary focus:outline-none focus:ring active:text-primary hover:text-white hover:bg-primary transition duration-500"
+              className="w-fit self-center mt-2 block rounded-full border border-primary bg-white px-4 py-3 text-center text-sm font-medium text-primary hover:ring-1 hover:ring-primary focus:outline-none focus:ring active:text-primary hover:text-white hover:bg-primary transition duration-500"
             >
               Get Started
             </button></div>
