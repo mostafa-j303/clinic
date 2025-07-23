fd464cd741146100615f30eb821fecce43a9b746
mostafa-j303 <mostafajarjour18@gmail.com>
2025-07-23 23:17:33 +0300

    backgound

--- src/app/_components/OurSevrices.tsx
+++ src/app/_components/OurSevrices.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import data from "../../../public/data.json"
 
 function OurSevrices() {
   return (
     <div>
-      <section className="bg-gradient-to-b from-hovprimary via-primary to-hovsecondary text-white">
+      <section className="bg-gradient-to-b from-hovprimary via-primary to-hovsecondary text-white"
+      style={
+          data?.images?.background
+            ? {
+                backgroundImage: `url(${data.images.background2})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundColor: "white",
+              }
+            : undefined
+        }
+      >
         <div className="  px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
           <div className="bg-white text-black p-5 border-hovprimary border-4 rounded-xl max-w-xl">
             <h2 id="aboutus" className="text-3xl font-bold sm:text-4xl">
@@ -20,9 +32,9 @@ function OurSevrices() {
             </p>
           </div>
 
-          <div className=" mt-8 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3 justify-center items-center">
+          <div className=" mt-8 grid grid-cols-1 items-start gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3 justify-center">
             <div className=" flex items-start gap-1">
-              <span className="shrink-0 rounded-lg bg-secondary text-black p-4">
+              {/* <span className="shrink-0 rounded-lg bg-secondary text-black p-4">
                 <svg
                   className="h-5 w-5"
                   fill="none"
@@ -39,7 +51,7 @@ function OurSevrices() {
                     d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
                   ></path>
                 </svg>
-              </span>
+              </span> */}
 
               <div className="bg-white border-4 border-hovprimary  p-3 rounded-lg">
                 <h2 className="text-lg text-black font-bold">
