ca62b1bd927676485b1857eaf4efef5fecff59a3
mostafa-j303 <mostafajarjour18@gmail.com>
2025-11-22 17:32:16 +0200

    misso pic

--- src/app/_components/Sparator.tsx
+++ src/app/_components/Sparator.tsx
@@ -8,7 +8,7 @@ interface SeparatorProps {
 
 const Separator: React.FC<SeparatorProps> = ({ value }) => {
   return (
-    <div className="text-white  relative text-4xl font-thin bg-gradient-to-b from-hovsecondary border-t-0 via-hovsecondary to-hovprimary flex flex-col items-center shadow-secondary   border-primary transition duration-500  ">
+    <div className="text-white  relative text-4xl font-thin bg-gradient-to-b from-hovprimary border-t-0 via-hovprimary to-hovsecondary flex flex-col items-center shadow-secondary   border-primary transition duration-500  ">
         <div className={' flex justify-center  items-center  animate-none pt-5 pl-7 pr-7 w-fit p-3 bg-primary  border-secondary border-r-[10px] border-l-[10px] rounded-t-[70px] skew-x-12 '}>
              {value}
         </div> 
